36eff5007e9d0e1afbf17b9a661d04cead9f0edf
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-07 12:46:31 -0500

    Agregada una impresion de mi nombre

--- firstJava.java
+++ firstJava.java
@@ -1,6 +1,10 @@
 public class firstJava {
 	public static void main(String[] args) {
+		
 		System.out.println("Hola mundo!");
+
 		System.out.println("\nBienvenido(a) al laboratorio de Estructuras de datos turno D :D");
+
+		System.out.println("\nMi nombre es Mauricio Eduardo Zegarra Puma");
 	}
 }
