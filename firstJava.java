1f6e5ac014a945b4db5cb6e94883bcd7061f5059
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-07 12:40:10 -0500

    Agregada la bienvenida

--- firstJava.java
+++ firstJava.java
@@ -1,5 +1,6 @@
 public class firstJava {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
+		System.out.println("\nBienvenido(a) al laboratorio de Estructuras de datos turno D :D");
 	}
 }
